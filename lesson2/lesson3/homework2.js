4d6c7b274e545d742c52c221774b895e35d9505d
Alice-1lis <lisenok2825@gmail.com>
2026-04-29 21:02:56 +0200

    Homework_3

---
+++ lesson2/lesson3/homework2.js
@@ -0,0 +1,7 @@
+// Homework 3.2
+
+let name = prompt("Як твоє ім'я?");
+let age = +prompt("Сколько тебе лет?");
+let city = prompt("В якому місті ти живеш?");
+
+console.log(`${name} ${age} ${city}`);
---
+++ lesson2/lesson3/homework3.js
@@ -0,0 +1,6 @@
+// Homework 3.3
+
+
+let number = 10369;
+let result = number.toString().split('').join(' ');
+console.log(result);
