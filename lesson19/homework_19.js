2fa1595f6682534be8418da7ef918d579144f918
Alice-1lis <lisenok2825@gmail.com>
2026-07-08 23:46:39 +0200

    homework19

--- lesson19/homework_19.js
+++ lesson19/homework_19.js
@@ -1,30 +1,28 @@
-const API_KEY = import.meta.env.API_KEY;
-const city = 'Kyiv';
-const button = document.getElementById('getWeatherBtn');
+const apiKeyWeather = 'cf618c05bb03c13715c40bc8fd1d861b';
+const cityWeather = 'Київ';
+const UrlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityWeather}&units=metric&appid=${apiKeyWeather}&lang=uk`;
 
-
-async function getWeather() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-     let weatherInfo = `Temperature in ${city}: ${temperature}°C, ${description}`;
-}
-   try {
-        const response = await fetch(url);
+async function getUpdateWeather() {
+    console.log('Клік зафіксовано, роблю запит...');
+    try {
+        const response = await fetch(UrlWeather, { cache: 'no-store' });
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
         const data = await response.json();
-        const temperature = data.main.temp;
-        const description = data.weather[0].description;
-      
-        document.getElementById('weatherInfo').textContent = weatherInfo;
-        weatherInfo = `Temperature in ${city}: ${temperature}°C, ${description}`;
+        document.querySelector('.temperature').textContent = `Температура ${cityWeather}: ${data.main.temp}°C`;
+        document.querySelector('.weather').textContent = `Погода: ${data.weather[0].description}`;
+        document.querySelector('.humidity').textContent = `Вологість: ${data.main.humidity}%`;
+        document.querySelector('.pressure').textContent = `Тиск: ${data.main.pressure} hPa`;
+        document.querySelector('.wind').textContent = `Швидкість вітру: ${data.wind.speed} m/s`;
+
+        console.log('Weather data updated successfully');
     }
-   catch (error) {
+    catch (error) {
         console.error('Error fetching weather data:', error);
     }
 
 }
-
-button.addEventListener('click', () => {
-    getWeather();
-});
+document.querySelector('.update-weather-btn').addEventListener('click', getUpdateWeather);
+getUpdateWeather();
 
 
 
